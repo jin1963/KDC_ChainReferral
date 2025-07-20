afd438908182883ba55bda4dfe0d8158f014f1a0
jin1963 <kaojino89@gmail.com>
2025-07-20 18:48:40 +0700

    Add files via upload

---
+++ abi.js
@@ -0,0 +1,16 @@
+const contractABI = [
+  {
+    "inputs": [{"internalType": "uint256","name": "usdtAmount","type": "uint256"}],
+    "name": "buyWithReferral",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [{"internalType": "address","name": "_referrer","type": "address"}],
+    "name": "registerReferrer",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
