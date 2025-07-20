ea53771645de1e75113b967d6a766ec348376005
jin1963 <kaojino89@gmail.com>
2025-07-20 18:36:59 +0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// config.js
-const contractAddress = "0x25D071772bd2Dc0f5ACe31631fa25f8A4B0bdcF2"; // ReferralKJC Contract
-const usdtAddress = "0x55d398326f99059fF775485246999027B3197955"; // USDT (BEP-20) บน BSC
-const routerAddress = "0x10ED43C718714eb63d5aA57B78B54704E256024E"; // PancakeSwap v2 Router
-const chainId = 56; // BNB Smart Chain Mainnet
