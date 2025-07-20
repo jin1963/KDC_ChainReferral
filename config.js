c3b3c5f364165148a5358698525e70593a8e3804
jin1963 <kaojino89@gmail.com>
2025-07-20 14:49:36 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+
+const contractAddress = "0x25D071772bd2Dc0f5ACe31631fa25f8A4B0bdcF2"; // ReferralKJC
+const usdtAddress = "0x55d398326f99059fF775485246999027B3197955"; // USDT บน BSC
+const kjcAddress = "0x62271BA79E2809091CF393eC1F313123271cEe2B"; // KJC Token
+const routerAddress = "0x10ED43C718714eb63d5aA57B78B54704E256024E"; // PancakeSwap v2 Router
+const chainId = 56; // BNB Smart Chain
