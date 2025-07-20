9380395874b26ad9a0bc4de4d2b53e1e1f314d4a
jin1963 <kaojino89@gmail.com>
2025-07-20 23:46:06 +0700

    Add files via upload

---
+++ usdtAbi.js
@@ -0,0 +1,12 @@
+const usdtABI = [
+  {
+    "constant": false,
+    "inputs": [
+      {"name": "_spender", "type": "address"},
+      {"name": "_value", "type": "uint256"}
+    ],
+    "name": "approve",
+    "outputs": [{"name": "", "type": "bool"}],
+    "type": "function"
+  }
+];
